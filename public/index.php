b27f99c4267a12bcd8390b295f28b832cc85dd82
oydm <389602549@qq.com>
2018-04-03 10:38:55 +0800

    cas login and logout

--- public/index.php
+++ public/index.php
@@ -4,6 +4,16 @@ define("YAF", 1);
 define('STARTTIME', microtime(true));
 define('WS_EXCEPTION', 10000);
 
+define('SYS_CAS_CLIENT',        'cas-client');
+define('SYS_DISCUZ',            'discuz');
+define('SYS_CACTI',             'cacti');
+define('SYS_RACKTABLES',        'racktables');
+define('SYS_OTRS',              'otrs');
+define('SYS_ZABBIX',            'zabbix');
+define('SYS_CMDB',            'ws-cmdb');
+
+include_once APP_PATH . '/CasClient.php';
+
 $application = new Yaf_Application( APP_PATH . "/conf/application.ini");
 $application->bootstrap()->run();
 echo '<!--' . round((microtime(true) - STARTTIME) * 1000) . 'ms-->';
