ea9b8f40b4cad8d51c36da6da0687d8dfea2b795
Carl <ee@aa.com>
2018-04-08 18:34:19 +0800

    1. DBModel 增加获取表字段的方法 2. 增加了一个用layui扩展的公共模块 comm.js 3. gf_ajax_return 方法增加 json header

--- application/modules/tools/controllers/SwitchScript.php
+++ application/modules/tools/controllers/SwitchScript.php
@@ -18,16 +18,18 @@ class SwitchScriptController extends BaseController {
                 $article_info = $this->_extractArticleInfo($article);
                 $this->assign('article_info', $article_info);
                 // 输出脚本
-                $switch_script = $this->_renderScript($article_info);
-                $this->assign('switch_script', $switch_script);
+                try {
+                    $switch_script = $this->_renderScript($article_info);
+                    $this->assign('switch_script', $switch_script);
+                } catch (WSException $e) {
+                    $this->assign('template_err', $e->getMessage());
+                }
             }
         }
     }
 
 
     public function _extractArticleInfo($article) {
-//        gf_dump($article['text']);
-
         // todo 具体实现等客服模板
         preg_match_all('/^(.*?)[:：](.*)$/im', $article['text'], $matches);
         $res = [];
@@ -35,8 +37,8 @@ class SwitchScriptController extends BaseController {
             $res[trim($m)] = trim($matches[2][$k]);
         }
         // gf_dump($res);
-        // return $res;
-        return [
+        // 模拟结果
+        $res = [
             'ip' => '127.0.0.1',
             '端口' => 'Ethernet0',
             '操作类型' => '关端口',
@@ -44,8 +46,15 @@ class SwitchScriptController extends BaseController {
             'http' => '//cloud.ws-cmdb.com:8089/',
             '垃圾信息' => '垃圾信息不会被显示',
         ];
-    }
 
+//        $valid = ['ip', '端口', '操作类型', 'extra', 'any'];
+//        foreach ($res as $k=>$v) {
+//            if (!in_array($k, $valid)) {
+//                unset($res[$k]);
+//            }
+//        }
+        return $res;
+    }
 
     public function _renderScript($article_info) {
         // 操作类型
@@ -54,8 +63,12 @@ class SwitchScriptController extends BaseController {
 //            switch ($script_template['content_type']) {
 //
 //            }
+            if (!$script_template) {
+                throw new WSException('无法识别的操作类型：'.$article_info['操作类型']);
+            }
             return gf_render_template($script_template['content'], $article_info);
         }
+        throw new WSException('无法从工单获取操作类型');
     }
 
 
